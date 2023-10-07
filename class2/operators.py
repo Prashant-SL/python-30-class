637d97ce3f4c4796e65b920030e18e1a74c85664
Prashant-SL <prashanth@IBOs-MacBook-Pro-3.local>
2023-10-07 14:10:36 +0530

    Class 1 & 2 of python. Learnt variables and operators in python

---
+++ class2/operators.py
@@ -0,0 +1,13 @@
+a = (10+20)
+b = (120-100)
+c = (120 != 20)
+hello = "hello"
+world = "world"
+
+p = True
+
+print(a)
+print(b)
+print(c)
+print(hello+" "+world)
+print(not (p))
---
+++ class2/variables.py
@@ -0,0 +1,27 @@
+# Variables
+x = 5
+y = "Prashant"
+z = [1, 2, 3]
+a = {y: "apple", x: "ball"}
+
+m, n, o = 100, 200, 300
+r = s = t = 100
+
+print(x)
+print(type(y))
+print(type(z))
+print(a)
+print(m)
+print(n)
+print(o)
+print(r)
+print(s)
+print(t)
+print()
+print(type(t))
+print()
+print(m, n, o)
+
+
+tpl = (1, 2, 3)
+print(tpl)
